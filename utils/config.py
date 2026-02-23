78e5da39cfb871d3f9076e8df20354d3d80411d4
Benjamin Baisden <wise9mind.solutions@gmail.com>
2026-02-23 02:20:03 -0500

    Restore LOGIN_PATH in config

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,8 @@
-# utils/config.py
+import os
 
-BASE_URL = "https://the-internet.herokuapp.com"
-LOGIN_PATH = "/login"
+BASE_URL = os.getenv("BASE_URL", "https://the-internet.herokuapp.com")
+LOGIN_PATH = os.getenv("LOGIN_PATH", "/login")
+
+HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
+BROWSER = os.getenv("BROWSER", "chromium")  # chromium | firefox | webkit
+SLOW_MO = int(os.getenv("SLOW_MO", "0"))
